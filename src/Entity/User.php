c08e68ec866964173ff7c126084cd22b682b7b47
Leny Bernard <leny@troopers.email>
2018-09-11 17:11:57 +0200

    add firstname, lastname and picture to user Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,6 +49,26 @@ class User implements UserInterface
      */
     private $enabled;
 
+    /**
+     * @ORM\Column(type="string", length=55, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=55, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $picture;
+
+    public function __toString(): ?string
+    {
+        return $this->firstname;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -156,6 +176,42 @@ class User implements UserInterface
     public function setEnabled(bool $enabled): self
     {
         $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
 
         return $this;
     }
--- src/Security/GitlabUserProvider.php
+++ src/Security/GitlabUserProvider.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use HWI\Bundle\OAuthBundle\OAuth\Response\PathUserResponse;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\VarDumper\VarDumper;
 
 class GitlabUserProvider extends OAuthUserProvider
@@ -15,10 +16,11 @@ class GitlabUserProvider extends OAuthUserProvider
     private $userRepository;
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager, UserRepository $userRepository)
+    public function __construct(EntityManagerInterface $entityManager, UserRepository $userRepository, $publicDir)
     {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->publicDir = $publicDir;
     }
 
     /**
@@ -40,6 +42,10 @@ class GitlabUserProvider extends OAuthUserProvider
         if (null === $user) {
             $user = new User();
             $user->setEmail($email);
+            $user->setFirstname($response->getData()['name']);
+            $filename = sprintf("/uploads/%s.jpeg", $response->getData()['username']);
+            file_put_contents($this->publicDir.$filename, fopen($response->getData()['avatar_url'], 'r'));
+            $user->setPicture($filename);
             $user->setEnabled(true);
             $user->setPassword('---gitlab-oauth---');
             $this->entityManager->persist($user);
